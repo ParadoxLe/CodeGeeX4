8a46a7de76f4a9fe9bdb9ea9a2fa85e212f5a1dc
sole <18672385717@163.com>
2025-12-09 11:41:10 +0800

    清空模块

--- Model_Enhancer/gnn_enhance/__init__.py
+++
@@ -1,7 +0,0 @@
-from .core.gat import GenericGAT
-from .core.graph_builder import GenericCodeGraphBuilder
-from .core.utils import graph2nx, visualize_graph, batch_graphs
-
-# 数据集适配器（按需导入，弱依赖）
-from .adapter.mbpp_adapter import MBPPGATAdapter
-from .adapter.humaneval_adapter import HumanEvalGATAdapter
